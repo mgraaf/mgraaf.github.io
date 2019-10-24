1fd237c752d85b03ed1b8f6420d3a990e1637335
Mackie Graaf <mgraaf@nd.edu>
2019-10-24 19:10:07 -0400

    Animated, interactive colorblock wireframe is a go!

--- project-two/assets/draggable.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-$( function() {
-    $( "#cabbage-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
-    $("#rf1").draggable({ snap: ".rf1-box", snapMode: "inner"  });
-    $( "#gof1" ).draggable({ snap: ".gof1-box", snapMode: "inner"  });
-    $( "#pf1" ).draggable({ snap: ".pf1-box", snapMode: "inner"  });
-  });
-  });
