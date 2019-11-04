970c45a583137149d6b31f58c625bb9697d206a7
Mackie Graaf <mgraaf@nd.edu>
2019-11-04 18:04:53 -0500

    Grounded garden plots

---
+++ project-two-improved/assets/images-draggable.js
@@ -0,0 +1,9 @@
+$(document).ready(function (){
+$( function() {
+  $( "#carrot-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  $( "#cabbage-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  $( "#pea-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  $( "#corn-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  $( "#beet-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  });
+  });
