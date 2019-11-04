1353ab2fb847527f6fde82f012828872fd504514
Mackie Graaf <mgraaf@nd.edu>
2019-11-04 12:41:00 -0500

    Garden gets a facelift

---
+++ project-two-updated/assets/colorblock-draggable.js
@@ -0,0 +1,69 @@
+$(document).ready(function (){
+$( function() {
+    $( "#carrot-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+    $( "#cabbage-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+    $( "#pea-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+    $( "#corn-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+    $( "#beet-square" ).draggable({ snap: ".plot", snapMode: "inner"  });
+  });
+  });
+
+  $(document).ready(function (){
+  $( function() {
+      $( "#carrot-square2" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square3" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square4" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square5" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square6" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square7" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      $( "#carrot-square8" ).draggable({ snap: ".plot", snapMode: "inner"  });
+    });
+    });
+
+    $(document).ready(function (){
+    $( function() {
+        $( "#cabbage-square2" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square3" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square4" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square5" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square6" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square7" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        $( "#cabbage-square8" ).draggable({ snap: ".plot", snapMode: "inner"  });
+      });
+      });
+
+      $(document).ready(function (){
+      $( function() {
+          $( "#pea-square2" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square3" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square4" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square5" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square6" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square7" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          $( "#pea-square8" ).draggable({ snap: ".plot", snapMode: "inner"  });
+        });
+        });
+
+        $(document).ready(function (){
+        $( function() {
+            $( "#corn-square2" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square3" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square4" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square5" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square6" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square7" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            $( "#corn-square8" ).draggable({ snap: ".plot", snapMode: "inner"  });
+          });
+          });
+
+          $(document).ready(function (){
+          $( function() {
+              $( "#beet-square2" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square3" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square4" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square5" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square6" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square7" ).draggable({ snap: ".plot", snapMode: "inner"  });
+              $( "#beet-square8" ).draggable({ snap: ".plot", snapMode: "inner"  });
+            });
+            });
